975ca5cec20317c484fb199e39c0b31f7677e6ed
heifara BUVAL <heifara.buval@gmail.com>
2022-09-17 09:36:52 +0200

    prod 2

--- scripts/Date.js
+++ scripts/Date.js
@@ -1,6 +1,6 @@
 const updateTime = () => {
     let today = new Date();
-    let time = JSON.stringify(today.getHours()).length !== 2 ? "0" + today.getHours() : today.getHours() + ":" + JSON.stringify(today.getMinutes()).length !== 2 ? "0" + today.getMinutes() : today.getMinutes();
+    let time = today.getHours() + ":" +today.getMinutes()
     document.querySelector('#time').innerText = time.toString();
 };
 updateTime();
